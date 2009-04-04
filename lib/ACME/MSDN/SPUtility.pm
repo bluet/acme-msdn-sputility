f09a2983db4b78fe670ae71b2b95a7d7c72067c7
BlueT - Matthew Lien - 練喆明 <BlueT@BlueT.org>
2009-04-04 15:46:35 +0800

    added Perl6::Say in Makefile.PL to avoid make test error on darwin <http://www.nntp.perl.org/group/perl.cpan.testers/2009/04/msg3617464.html>

--- lib/ACME/MSDN/SPUtility.pm
+++ lib/ACME/MSDN/SPUtility.pm
@@ -13,11 +13,11 @@ ACME::MSDN::SPUtility - SPUtility.HideTaiwan Method (Microsoft.SharePoint.Utilit
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
